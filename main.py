b7a7f35b4b03bec2103cd68b232420d04d6dc308
withsj <withsj.in@gmail.com>
2020-12-18 11:45:55 +0530

    update code and give coments

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
-
+"""This code to detect it's Android or not 
+if it's not android than app window size change in android phone size"""
 from kivy.utils import platform
 if platform != 'android':
     from kivy.config import Config
     Config.set("graphics","width",360)
     Config.set("graphics","height",740)
+#--[End platform specific code]
 
+"""code for android keyboard. when in android keyboard show textbox 
+automatic go to top of keyboard so user can see when he type msg"""
 from kivy.core.window import Window
 Window.keyboard_anim_args = {"d":.2,"t":"in_out_expo"}
 Window.softinput_mode = "below_target"
+#--[End Msg Box code ]
 
+"""All imports for UI here Kivy,KivyMD or etc that help in UI"""
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
 from kivy.uix.floatlayout import FloatLayout
 from kivymd.uix.tab import MDTabsBase
 from kivymd.uix.list import TwoLineAvatarListItem,ImageLeftWidget
-from kivymd.uix.label import MDLabel
-from kivymd.uix.card import MDCard
-from kivymd.uix.button import MDRoundFlatButton
+# from kivymd.uix.label import MDLabel
+# from kivymd.uix.card import MDCard
+# from kivymd.uix.button import MDRoundFlatButton
+
+#--[End UI Imports]
+
 
 class HamsterApp(MDApp):
+    """
+    Hamster App start from here this class is root of app.
+    in kivy (.kv) file when use app.method_name app is start from here
+    """
 
     def change_screen(self,name):
         """
@@ -35,7 +48,8 @@ class HamsterApp(MDApp):
         
     def chat_textbox(self):
         """
-            Incress size of sending textbox accourting to text
+            MDCard size change when MSGbox use multilines.
+            MDCard y axis size incress when MSGbox y axis size incress
         """
         fixed_Y_size = screen_manager.get_screen("chat_room").ids.root_chatroom.size[1]/2
         msg_textbox=screen_manager.get_screen("chat_room").ids.msg_textbox.size
@@ -47,7 +61,8 @@ class HamsterApp(MDApp):
 
     def send_msg(self,msg_data):
         """
-        Use this function when you send msg 
+            When send button use to send msg this function call
+            and clear MSGbox 
         """
         # text_msg = MDLabel(text=msg_data,halign="right")
         # new_msg = text_msg
@@ -57,17 +72,22 @@ class HamsterApp(MDApp):
 
 
     def chat_room(self,touch,a):
-        """Switch to Chatroom """
+        """Switch to Chatroom. but username and chatroom username 
+        change according to which one you touch in chat list"""
+        
+        name = touch.text
+        screen_manager.get_screen("chat_room").ids.profile_bar.title = name
         self.change_screen("chat_room")
     
     def all_chats(self):
         """
-        Add all chats in chat tab
+        All Chat that show in home chat tab. all chat are added by 
+        this method. it will use in differe t in future.
         """
         # for dummy chats [------
         # self.change_screen("profile")
         twolineW= TwoLineAvatarListItem(text=f"Hamster",
-            secondary_text="Hamster is Chatting app",
+            secondary_text="@username",
             on_touch_up=self.chat_room)
 
         twolineW.add_widget(ImageLeftWidget(source="hamster_icon.png"))
@@ -76,10 +96,15 @@ class HamsterApp(MDApp):
         #  ----- ] end dummy chats
 
     def search_account(self,search_field):
+        """
+        this method use when search button pressed search_field
+        contain data in string that you want to search on hamster server
+        """
+
         # for dummy search item [------
         
         twolineW= TwoLineAvatarListItem(text=f"{search_field}",
-            secondary_text=f"Hamster text for {search_field}")
+            secondary_text=f"@{search_field}")
 
         twolineW.add_widget(ImageLeftWidget(source="hamster_icon.png"))
         
@@ -88,8 +113,18 @@ class HamsterApp(MDApp):
 
     
     def change_profile_img(self):
+        """
+        method call when image click on profile_view page.
+        if it's user own profile than show options of change.
+        """
         print("hello")
+
     def build(self):
+        """
+        This method call before on_start() method so anything
+        that need before start application all other method and code 
+        write here.
+        """
         self.theme_cls.theme_style="Light"
         global screen_manager
         screen_manager = ScreenManager()
@@ -102,7 +137,13 @@ class HamsterApp(MDApp):
         screen_manager.add_widget(Builder.load_file("ui//chat_room.kv"))
         
         return screen_manager
+    
     def on_start(self):
+        """
+        Anything we want to run when start application that code is here.
+        """
         self.all_chats()
+
 if __name__ == "__main__":
-    HamsterApp().run()
+    # Start application from here.
+    HamsterApp().run() 
