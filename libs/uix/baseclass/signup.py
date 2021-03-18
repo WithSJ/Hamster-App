9404e1af10173f3850153f5b475c141f19ee534d
Sandeep Jadam <withsj.in@gmail.com>
2021-03-18 11:58:01 +0530

    firebase signup

--- libs/uix/baseclass/signup.py
+++ libs/uix/baseclass/signup.py
@@ -1,7 +1,14 @@
 from main_imports import MDScreen
 from libs.applibs import utils
-
+from libs.applibs.firebase import authentication
 utils.load_kv("signup.kv")
 
 class Signup_Screen(MDScreen):
-    pass
+    def signup(self,email,fullname,username,password):
+        print(email,fullname,username,password)
+        print(
+                authentication.SignUp(
+                email= email, fullname= fullname,
+                username= username, password= password)
+            )
+        
